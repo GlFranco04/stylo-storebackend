5c711d06b1b23a28d8dd232c72447017c2ef181b
Gabriel Andres Lopez Franco <gabrielandreslopezfranco04@gmail.com>
2024-09-24 08:13:33 -0400

    Jwt ahora devuelve el rol más, eso sirve para controlar los visuals del front

--- src/main/java/com/stylo/store/utils/JwtUtil.java
+++ src/main/java/com/stylo/store/utils/JwtUtil.java
@@ -2,12 +2,16 @@ package com.stylo.store.utils;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.function.Function;
+import java.util.Map;
+import java.util.HashMap;
 
 @Component
 public class JwtUtil {
@@ -41,16 +45,23 @@ public class JwtUtil {
     }
 
     public String generateToken(UserDetails userDetails) {
-        return createToken(userDetails.getUsername());
-    }
+        Map<String, Object> claims = new HashMap<>();
+        
+        // Agregar el rol como un "claim" al token
+        String role = userDetails.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .findFirst()
+                    .orElse(null);  // Si el usuario tiene un rol, se obtiene el primero
 
-    private String createToken(String subject) {
+        claims.put("role", role);  // Añadir el rol al token
+
         return Jwts.builder()
-                   .setSubject(subject)
-                   .setIssuedAt(new Date(System.currentTimeMillis()))
-                   .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 horas
-                   .signWith(SECRET_KEY)
-                   .compact();
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername()) // El subject es el nombre del usuario (o email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SECRET_KEY) // Reemplaza "secretKey" con tu llave secreta
+                .compact();
     }
 
     public Boolean validateToken(String token, UserDetails userDetails) {
