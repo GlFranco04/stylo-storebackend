53b5592805270fd67ea51a762db64292e6941908
Gabriel Andres Lopez Franco <gabrielandreslopezfranco04@gmail.com>
2024-09-30 10:46:33 -0400

    Prueba deploy 10

--- src/main/java/com/stylo/store/config/WebConfig.java
+++ src/main/java/com/stylo/store/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  // Aplica CORS a las rutas de la API
-            .allowedOrigins("https://stylo-storebackend-production.up.railway.app")  // Permite el frontend
+            .allowedOrigins("https://stylo-store-a214xo7q4-gabriels-projects-9c5cda58.vercel.app")  // Permite el frontend de Vercel
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")  // Métodos permitidos
             .allowedHeaders("*")  // Permite todos los headers
             .allowCredentials(true);  // Si usas cookies o autenticación
