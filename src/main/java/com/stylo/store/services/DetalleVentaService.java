68f4844132d1cbfa8f838c7cf6b3c2540ea6ca13
Gabriel Andres Lopez Franco <gabrielandreslopezfranco04@gmail.com>
2024-10-01 01:55:32 -0400

    Prueba NotaVenta e Inventario 1, NotaCompra e Inventario funcionando

--- src/main/java/com/stylo/store/services/DetalleVentaService.java
+++ src/main/java/com/stylo/store/services/DetalleVentaService.java
@@ -1,9 +1,12 @@
 package com.stylo.store.services;
 
 import com.stylo.store.models.DetalleVenta;
+import com.stylo.store.models.Inventario;
+import com.stylo.store.models.NotaVenta;
 import com.stylo.store.repositories.DetalleVentaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +16,10 @@ public class DetalleVentaService {
 
     @Autowired
     private DetalleVentaRepository detalleVentaRepository;
+    @Autowired
+    private NotaVentaService notaVentaService;
+    @Autowired
+    private InventarioService inventarioService;
 
     // Obtener todos los detalles de venta
     public List<DetalleVenta> obtenerTodosLosDetallesVenta() {
@@ -27,6 +34,59 @@ public class DetalleVentaService {
     // Crear o actualizar un nuevo detalle de venta
     public DetalleVenta saveDetalleVenta(DetalleVenta detalleVenta) {
         return detalleVentaRepository.save(detalleVenta);
+    }
+
+@Transactional
+public DetalleVenta saveDetalleCompra(DetalleVenta detalleVenta) {
+    // Validar que NotaCompra y DetalleProducto no sean nulos
+    if (detalleVenta.getNotaVenta() == null || detalleVenta.getNotaVenta().getId() == null) {
+        throw new IllegalArgumentException("La NotaVenta no puede ser nula y debe tener un ID válido.");
+    }
+    if (detalleVenta.getDetalleProducto() == null || detalleVenta.getDetalleProducto().getId() == null) {
+        throw new IllegalArgumentException("El DetalleProducto no puede ser nulo y debe tener un ID válido.");
+    }
+
+    // Recuperar la NotaCompra desde la base de datos para asegurarse de que todas las relaciones estén cargadas
+    Optional<NotaVenta> optionalNotaVenta = notaVentaService.obtenerNotaVentaPorId(detalleVenta.getNotaVenta().getId());
+    if (!optionalNotaVenta.isPresent()) {
+        throw new IllegalArgumentException("NotaVenta no encontrada con el ID proporcionado: " + detalleVenta.getNotaVenta().getId());
+    }
+
+    NotaVenta notaVenta = optionalNotaVenta.get();
+    
+    // Validar que la NotaVenta tenga una Sucursal asociada
+    if (notaVenta.getSucursal() == null) {
+        throw new IllegalArgumentException("La Sucursal en la NotaVenta no puede ser nula.");
+    }
+
+    // Reasignar la NotaCompra cargada completamente a detalleCompra
+    detalleVenta.setNotaVenta(notaVenta);
+
+    // Guardar primero el detalleCompra para asegurar que todos los objetos estén persistidos
+    DetalleVenta savedDetalleVenta = detalleVentaRepository.save(detalleVenta);
+
+    // Buscar el inventario correspondiente al detalle del producto usando la sucursal de la nota de compra
+    Optional<Inventario> optionalInventario = inventarioService.findInventarioBySucursalAndDetalleProducto(
+                    savedDetalleVenta.getNotaVenta().getSucursal(),
+                    savedDetalleVenta.getDetalleProducto());
+
+    Inventario inventario;
+    if (optionalInventario.isPresent()) {
+        inventario = optionalInventario.get();
+    } else {
+        throw new IllegalArgumentException("Inventario no existente");
+    }
+
+    // Restar la cantidad comprada del inventario disponible
+    Long nuevaCantidadDisponible = inventario.getInventarioDisponible() + savedDetalleVenta.getCantidad();
+    inventario.setInventarioDisponible(nuevaCantidadDisponible);
+    if (nuevaCantidadDisponible < 0 ) {
+        throw new IllegalArgumentException("Inventario insuficiente para el producto: " + savedDetalleVenta.getDetalleProducto().getId());
+    }
+    // Guardar el inventario actualizado
+    inventarioService.saveInventario(inventario);
+
+    return savedDetalleVenta;
     }
 
     // Eliminar un detalle de venta
