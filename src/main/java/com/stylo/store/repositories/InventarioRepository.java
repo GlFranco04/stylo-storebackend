2affd3afc9d97fd0c2c87ea37dc1d9c324bde5f7
Gabriel Andres Lopez Franco <gabrielandreslopezfranco04@gmail.com>
2024-10-01 00:10:50 -0400

    Eliminada Prueba NotaCompra e Inventario

--- src/main/java/com/stylo/store/repositories/InventarioRepository.java
+++ src/main/java/com/stylo/store/repositories/InventarioRepository.java
@@ -1,16 +1,11 @@
 package com.stylo.store.repositories;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.stylo.store.models.Inventario;
-import com.stylo.store.models.Sucursal;
-import com.stylo.store.models.DetalleProducto;
 
 @Repository
-public interface InventarioRepository extends JpaRepository<Inventario, Long> {
-    // Método para encontrar el inventario por sucursal y detalle del producto
-    Optional<Inventario> findBySucursalAndDetalleProducto(Sucursal sucursal, DetalleProducto detalleProducto);
+public interface InventarioRepository extends JpaRepository<Inventario,Long> {
+  // Metodos personalizados si se necesitan
 }
--- src/main/java/com/stylo/store/services/NotaCompraService.java
+++ src/main/java/com/stylo/store/services/NotaCompraService.java
@@ -7,18 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.stylo.store.models.NotaCompra;
-import com.stylo.store.models.DetalleCompra;
-import com.stylo.store.models.Inventario;
 import com.stylo.store.repositories.NotaCompraRepository;
-import com.stylo.store.services.InventarioService;
 
 @Service
 public class NotaCompraService {
     @Autowired
     private NotaCompraRepository notaCompraRepository;
-
-    @Autowired
-    private InventarioService inventarioService;
 
     // Obtener todas las notas de venta
     public List<NotaCompra> obtenerTodasLasNotasCompra() {
@@ -32,29 +26,8 @@ public class NotaCompraService {
 
     // Crear o actualizar una nueva nota de venta
     public NotaCompra saveNotaCompra(NotaCompra notaCompra) {
-        // Guardar la nota de compra
-        NotaCompra savedNotaCompra = notaCompraRepository.save(notaCompra);
-
-        // Iterar sobre los detalles de compra para actualizar el inventario disponible
-        for (DetalleCompra detalleCompra : savedNotaCompra.getDetalleCompras()) {
-            // Obtener el inventario correspondiente al detalle del producto
-            Optional<Inventario> optionalInventario = inventarioService
-                    .findInventarioBySucursalAndDetalleProducto(savedNotaCompra.getSucursal(), detalleCompra.getDetalleProducto());
-
-            if (optionalInventario.isPresent()) {
-                Inventario inventario = optionalInventario.get();
-                // Restar la cantidad comprada al inventario disponible
-                Long nuevaCantidadDisponible = inventario.getInventarioDisponible() - detalleCompra.getCantidad();
-                inventario.setInventarioDisponible(nuevaCantidadDisponible);
-
-                // Guardar el inventario actualizado
-                inventarioService.saveInventario(inventario);
-            }
-        }
-
-        return savedNotaCompra;
+        return notaCompraRepository.save(notaCompra);
     }
-
     // Eliminar una nota de venta
     public void eliminarNotaCompra(Long id) {
         notaCompraRepository.deleteById(id);
